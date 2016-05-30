768a1d1d5bcac5918df873994d0f49057335a78e
misoton665 <misoton998@hotmail.co.jp>
2016-05-30 13:53:45 +0900

    Add Member and Owner classes

--- app/services/User.scala
+++ app/services/User.scala
@@ -1,3 +1,17 @@
 package services
 
-case class User(userId: String, userName: String)
+case class User(id: String, name: String)
+
+class Member(id: String, name: String) {
+  val isOwner: Boolean = false
+}
+
+object Member {
+  def apply(id: String, name: String): Member = {
+    new Member(id, name)
+  }
+}
+
+case class Owner(id: String, name: String) extends Member(id, name){
+  override val isOwner = true
+}
