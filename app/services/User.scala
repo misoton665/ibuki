1af80fe86c126cb2880bb90e786c033e9c2300cc
misoton665 <misoton998@hotmail.co.jp>
2016-05-30 14:07:25 +0900

    Add a email parameter for User and its sub classed

--- app/services/User.scala
+++ app/services/User.scala
@@ -1,17 +1,29 @@
 package services
 
-case class User(id: String, name: String)
+class User(id: String, name: String, email: Email) {
 
-class Member(id: String, name: String) {
+}
+
+object User {
+  def apply(id: String, name: String, email: Email): User = {
+    new User(id, name, email)
+  }
+}
+
+case class Email(email: String) {
+  val isValidEmail: Boolean = email.matches("""^[0-9a-z_./?-]+@([0-9a-z-]+\.)+[0-9a-z-]+$""")
+}
+
+class Member(id: String, name: String, email: Email) extends User(id, name, email){
   val isOwner: Boolean = false
 }
 
 object Member {
-  def apply(id: String, name: String): Member = {
-    new Member(id, name)
+  def apply(id: String, name: String, email: Email): Member = {
+    new Member(id, name, email)
   }
 }
 
-case class Owner(id: String, name: String) extends Member(id, name){
+case class Owner(id: String, name: String, email: Email) extends Member(id, name, email) {
   override val isOwner = true
 }
