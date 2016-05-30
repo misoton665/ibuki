897c1ae057232da207020d9643d3001651952a40
misoton665 <misoton998@hotmail.co.jp>
2016-05-30 14:58:15 +0900

    Add a test for service that is main content of the application

--- test/ServiceSpec.scala
+++ test/ServiceSpec.scala
@@ -1,6 +1,18 @@
-/**
-  * Created by misoton on 2016/05/30.
-  */
-class ServiceSpec {
+import org.scalatest.{FlatSpec, Matchers}
+import services.{Activity, Email, Member, RootAction}
+
+class ServiceSpec extends FlatSpec with Matchers {
+
+  val emailModel: Email = Email("misoton998@ibuki.com")
+
+  val memberModel: Member = Member("model_id", "model_name", emailModel)
+
+  val rootActionModel: RootAction = RootAction(memberModel, Array())
+
+  val activityModel: Activity = Activity(Array(rootActionModel))
+
+  "A head of Activity" should "be RootAction" in {
+    activityModel.rootAction should be(Some(rootActionModel))
+  }
 
 }
