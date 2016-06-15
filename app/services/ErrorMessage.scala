17aafab3a226eaa6a15981a133c9a1a5b7b4ff0f
misoton665 <misoton998@hotmail.co.jp>
2016-06-15 20:50:03 +0900

    Rename file from ErrorMessageGenerator.scala to ErrorMessage.scala

--- app/services/ErrorMessage.scala
+++ app/services/ErrorMessage.scala
@@ -2,15 +2,16 @@ package services
 
 import play.api.libs.json.{JsValue, Json}
 
-object ErrorMessageGenerator {
+object ErrorMessage {
   val keyErrorMessage = "errorMessage"
   val INVALID_JSON = "invalid json"
+  val DB_ERROR = "DB error"
 
   case class ErrorMessage(json: JsValue)
 
   def generateError(errorMessage: String): ErrorMessage = {
     ErrorMessage(
-      Json.obj(keyErrorMessage -> INVALID_JSON)
+      Json.obj(keyErrorMessage -> errorMessage)
     )
   }
 }
