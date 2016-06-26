96d1c187e4847870441db089a704003f0169c2ff
misoton <misoton998@gmail.com>
2016-06-26 19:21:40 +0900

    Remove import in two Specs

--- test/ServiceSpec.scala
+++ test/ServiceSpec.scala
@@ -1,9 +1,6 @@
 import org.scalatest.{FlatSpec, Matchers}
-import play.api.libs.json.{JsValue, Json}
-import .AttributeTag
-import services.Contribution.{Action, Activity, DocumentAction, RootAction}
-import services.DateConverter
 
 class ServiceSpec extends FlatSpec with Matchers {
+
 
 }
