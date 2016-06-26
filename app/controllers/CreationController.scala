b2704d83666cafab703c48dfda5f47e9ad7e241b
misoton665 <misoton998@hotmail.co.jp>
2016-06-26 09:59:37 +0900

    Update CreationController to add explanation for createAction API

--- app/controllers/CreationController.scala
+++ app/controllers/CreationController.scala
@@ -2,7 +2,7 @@ package controllers
 
 import javax.inject.Inject
 
-import play.api.libs.json.JsValue
+import play.api.libs.json.{JsResult, JsValue}
 import play.api.mvc.{Action, BodyParsers, Controller}
 import services.ErrorMessage
 
@@ -18,14 +18,42 @@ class CreationController @Inject() extends Controller {
       // /create/action
       case ("action", js)   => createAction(js)
 
-      // otherwise it will occur error that an api not found.
-      case _          => ErrorMessage.generateError(ErrorMessage.API_NOT_FOUND).json.toString()
+      // otherwise it will be occurred error that an api not found.
+      case _ => ErrorMessage.generateError(ErrorMessage.API_NOT_FOUND).json.toString()
     }
 
     Ok(result)
   }
 
+  /**
+    * createActivity (/contribution/activity)
+    *
+    * parameter JSON
+    *  activity_name: string - Activity name
+    *  user_id: string       - Contributor name
+    *  group_id: string      - A group which the activity is going to contributed to
+    *
+    * return JSON
+    * OK:
+    *   activity_id: string - Created activity ID
+    *   date: string        - A Date when activity created
+    *
+    * NG:
+    *   Error message JSON by ErrorMessage
+    *
+    * @param json JSON string by request body
+    * @return return JSON
+    */
   private def createActivity(json: JsValue): String = {
+    val parameterKeys = List("activity_name", "user_id", "group_id")
+    val parameters: List[JsResult[String]] = parameterKeys.map { key =>
+      (json \ key).validate[String]
+    }
+    val validJson: Boolean = !parameters.contains((res: JsResult[String]) => res.isError)
+
+    if (validJson) {
+
+    }
     "activity"
   }
 
