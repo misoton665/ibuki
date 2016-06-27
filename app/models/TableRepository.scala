f09fbfd2f446a8036b22477d6feb6dd00c77d3ea
misoton665 <misoton998@hotmail.co.jp>
2016-06-27 22:42:39 +0900

    Update TableRepository for remove unused type parameter A and replace from B

--- app/models/TableRepository.scala
+++ app/models/TableRepository.scala
@@ -13,8 +13,8 @@ class TableRepository @Inject()(protected val dbConfigProvider: DatabaseConfigPr
 
   import dbConfig.driver.api._
 
-  def findBySomething[A <: slick.lifted.Rep[B], B <: slick.driver.PostgresDriver.api.Table[R], R]
-    (comp: (B) => slick.lifted.Rep[Boolean])
-    (implicit tableQuery: slick.lifted.TableQuery[B]): Future[List[R]]
+  def findBySomething[A <: slick.driver.PostgresDriver.api.Table[R], R]
+    (comp: (A) => slick.lifted.Rep[Boolean])
+    (implicit tableQuery: slick.lifted.TableQuery[A]): Future[List[R]]
     = db.run(tableQuery.filter[slick.lifted.Rep[Boolean]](comp).to[List].result)
 }
