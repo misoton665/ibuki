d150cba893db971fe6be062e2d9f09b03765d1f7
misoton665 <misoton998@hotmail.co.jp>
2016-06-28 02:32:26 +0900

    Add IbukiGroupRepo

---
+++ app/models/IbukiGroupRepo.scala
@@ -0,0 +1,32 @@
+package models
+
+import javax.inject.Inject
+
+import models.Tables.{IbukiGroup, IbukiGroupRow}
+import play.api.db.slick.DatabaseConfigProvider
+import services.DateConverter
+
+class IbukiGroupRepo @Inject()(override protected val dbConfigProvider: DatabaseConfigProvider)
+  extends TableRepository[IbukiGroup, IbukiGroupRow](dbConfigProvider) {
+
+  import dbConfig.driver.api._
+
+  implicit val IbukiGroups = TableQuery[IbukiGroup]
+
+  def create = insert(_.id) _
+
+  def findById(id: Int) = findBy(_.id === id)
+
+  def findByGroupId(groupId: String) = findBy(_.groupId === groupId)
+
+  def findByOwnerId(ownerId: String) = findBy(_.ownerId === ownerId)
+
+  def findByDate(date: java.sql.Date) = findBy(_.date === date)
+
+  def createIbukiGroup(groupId: String, groupName: String, ownerId: String) = {
+    val date = DateConverter.generateNowDate
+    val newIbukiGroup = IbukiGroupRow(0, groupId, groupName, ownerId, date)
+
+    create(newIbukiGroup)
+  }
+}
